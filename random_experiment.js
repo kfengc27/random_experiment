13284746715a1d38b89077bad87d3a482b00f961
Kevin Feng <kfengc27@gmail.com>
2022-09-05 17:40:50 +1000

    Add files via upload

---
+++ random_experiment.js
@@ -0,0 +1,11 @@
+<script>
+var url_list = [
+        "https://lab5-for-john.herokuapp.com/join/risaheke",
+        "https://lab5-for-john.herokuapp.com/join/mepagagu",
+        "https://lab5-for-john.herokuapp.com/join/gepuzujo",
+        "https://lab5-for-john.herokuapp.com/join/honozuti",
+        "https://lab5-for-john.herokuapp.com/join/dijakiha",
+        "https://lab5-for-john.herokuapp.com/join/nusefeva"
+]
+window.location.href = url_list[Math.floor(Math.random() * url_list.length)];
+</script>
