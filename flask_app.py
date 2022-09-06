61a22e37ecd2421e51a9ef1df4099e45128f95ca
Kevin Feng <kfengc27@gmail.com>
2022-09-06 20:39:49 +1000

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -6,7 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    url_list = ['https://lab5-for-john.herokuapp.com/join/risaheke', "https://lab5-for-john.herokuapp.com/join/mepagagu", "https://lab5-for-john.herokuapp.com/join/gepuzujo","https://lab5-for-john.herokuapp.com/join/honozuti","https://lab5-for-john.herokuapp.com/join/dijakiha","https://lab5-for-john.herokuapp.com/join/nusefeva"]
+    url_list = [ "http://otree11.experiment.unsw.edu.au/join/savadoge",
+        "http://otree11.experiment.unsw.edu.au/join/mogeliva",
+        "http://otree11.experiment.unsw.edu.au/join/herebema",
+        "http://otree11.experiment.unsw.edu.au/join/japagaza"]
     return redirect(url_list[random.randint(0, 5)], code=302)
 
 if __name__ == '__main__':
