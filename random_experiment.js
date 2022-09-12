1617fd78911556ef3ff26c92c0f74d77ae227daa
Kevin Feng <kfengc27@gmail.com>
2022-09-12 21:22:59 +1000

    Update random_experiment.js

--- random_experiment.js
+++ random_experiment.js
@@ -1,7 +1,7 @@
 var url_list = [
-        "http://otree11.experiment.unsw.edu.au/join/savadoge",
-        "http://otree11.experiment.unsw.edu.au/join/mogeliva",
-        "http://otree11.experiment.unsw.edu.au/join/herebema",
-        "http://otree11.experiment.unsw.edu.au/join/japagaza"
+        "http://otree11.experiment.unsw.edu.au/join/rukenobe",
+        "http://otree11.experiment.unsw.edu.au/join/desunuto",
+        "http://otree11.experiment.unsw.edu.au/join/kajefide",
+        "http://otree11.experiment.unsw.edu.au/join/kitadotu"
 ]
 window.location.href = url_list[Math.floor(Math.random() * url_list.length)];
