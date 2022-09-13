9f5b9ba5f0e2fe6491b283aab57aa3b19d48165d
Kevin Feng <kfengc27@gmail.com>
2022-09-13 11:23:20 +1000

    Update random_experiment.js

--- random_experiment.js
+++ random_experiment.js
@@ -3,5 +3,6 @@ var url_list = [
         "http://otree11.experiment.unsw.edu.au/join/desunuto",
         "http://otree11.experiment.unsw.edu.au/join/kajefide",
         "http://otree11.experiment.unsw.edu.au/join/kitadotu"
+        
 ]
 window.location.href = url_list[Math.floor(Math.random() * url_list.length)];
